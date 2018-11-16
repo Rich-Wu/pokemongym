bb5f5eba14a94cca76a673ec0f4d412628d27268
Noah Dorce <noah.dorce65@gmail.com>
2018-11-16 11:16:53 -0500

    minor changes

--- js/library.js
+++ js/library.js
@@ -124,18 +124,18 @@ function setScreen(trainer) {
     }
 
     for (var i = 0; i < hp.length; i++) {
-      hp[i].innerHTML = `${trainer.pokemon[i].hp}`
+      hp[i].innerHTML = `${trainer.pokemon[i].hp}`;
     }
 
     for (var i = 0; i < def.length; i++) {
-      def[i].innerHTML = `${trainer.pokemon[i].def}`
+      def[i].innerHTML = `${trainer.pokemon[i].def}`;
     }
 
     for (var i = 0; i < atk.length; i++) {
-      atk[i].innerHTML = `${trainer.pokemon[i].atk}`
+      atk[i].innerHTML = `${trainer.pokemon[i].atk}`;
     }
 
     for (var i = 0; i < abilities.length; i++) {
-      abilities[i].innerHTML = `${trainer.pokemon[i].abilties}`
+      abilities[i].innerHTML = `${trainer.pokemon[i].abilties}`;
     }
 }
